8d3c09e654f5684ebf0836908d104df8276d1a85
Adarsh V Kapse <adarshvkapse@gmail.com>
2026-06-02 23:54:01 +0530

    Fix CORS for frontend

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+console.log("BACKEND_URL =", BACKEND_URL);
 export const API = `${BACKEND_URL}/api`;
 export const api = axios.create({ baseURL: API, timeout: 60000 });
 
