5ded3ee5ac6fa88d8e8fadfa1925856d364eec29
Adarsh V Kapse <adarshvkapse@gmail.com>
2026-06-03 20:36:07 +0530

    Changed frontend URL

--- backend/server.py
+++ backend/server.py
@@ -596,7 +596,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://network-digital-twin-production.up.railway.app",
+        "https://network-digital-twin.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
